7a32951ee417485790d69459ee838a481292af08
Raiyyan Mahmud Alif <69851090+RaiyyanAlif@users.noreply.github.com>
2026-05-01 12:13:03 +0600

    Update Example2.java

--- Day08/Example2.java
+++ Day08/Example2.java
@@ -4,7 +4,7 @@ public class Example2 {
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "Raiyyan");
-        map.put(2, "Tiha");
+        map.put(2, "Tafsir");
         map.put(3, "Mashrafee");
 
         for (Integer key : map.keySet()) {
